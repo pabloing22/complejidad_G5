13d5e2e5d5306f4c3c18e0c78767e9ee5662ec85
miguelbritos91 <miguelbritos91@gmail.com>
2019-06-17 17:36:30 -0300

    mejora de un 0,0000000009 % en el tiempo XD

--- 1_Números amigos/Versión JS/amigosIterativo.js
+++ 1_Números amigos/Versión JS/amigosIterativo.js
@@ -1,10 +1,11 @@
 console.time('loop');
 const start = Date.now();
-var n=1000000;
+var n=100000;
 list=[];
 var x = 0;
 var y = 0;
-for (var i = 1; i <= n; i++) {
+var i=1;
+while (i <= n) {
     var a=i;
     for (var j = 1; j < i; j++) {
         if(i%j==0){
@@ -12,7 +13,7 @@ for (var i = 1; i <= n; i++) {
         }
     }
     b=x;
-    if(a>b){
+    if(a!=b){
         for(var k=1; k<b; k++){
             if(b%k==0){
                 y+=k;
@@ -20,9 +21,18 @@ for (var i = 1; i <= n; i++) {
         }
     }
     if(a==y){
-        list.push(a+' es amigo de '+b);
+        list.push([a,b]);
+        // si existe un numero amigo a la variable i que controla el ciclo principal le pasamos el numero amigo mayor para evitar 
+        // controlar numeros amigos repetidos
+        if(a>b){
+            i=a;
+        }else{
+            i=b;
+        }
     }
-    x=0; y=0;
+    x=0; 
+    y=0;
+    i++; //aumentamos el ciclo en uno
 }
 const end=Date.now();
 console.timeEnd('loop');
