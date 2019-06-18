b07d979edfcfa70253c6b5f2c561aa24f64eb8fa
pablitoing <pablito@circulo.digital>
2019-06-17 21:37:58 -0300

    numeros amigos python

--- 1_Números amigos/Version PY/amigos.py
+++ 1_Números amigos/Version PY/amigos.py
@@ -1,14 +1,40 @@
-# print ('hola mundo')
-# Halla la suma de divisores de un numero donde i=numero y j=al maximo divisor del numero i
-def divisores(i, j):
-    print([i, j])
-    if(j<2):
-        print('entro por 1')
-        return j
-    elif ((i % j) == 0):
-        print('entro por 2')
-        return j+divisores(i,j-1)
-    else:
-        print('entro por 3')
-        return divisores(i,j-1)
+from decorators import delta_time
 
+
+def divisores(n):
+    sum=0
+    top=0
+    if(n % 2==0):
+        top=(n // 2)+1
+    elif(n % 3 == 0):
+        top=(n // 3)+1
+    elif (n % 5 == 0):
+        top=(n // 5)+1
+    elif (n % 7 == 0):
+        top=(n // 7)+1
+    elif (n % 11 == 0):
+        top=(n // 11)+1
+    else: 
+        top=(n // 2)+1
+    
+    for i in range(1,top):
+        if ((n%i)==0):
+            sum=sum+i
+    return sum
+
+
+@delta_time('Grupo 5')
+def amigos(n):
+    founded=[]
+    amigos=[]
+    for i in range(219,n):
+        if not(i in founded):
+            x=divisores(i)
+            if not(x in founded):
+                y=divisores(x)
+                if (i!=x) and (i==y):
+                    amigos.append([i,x])
+                    founded.append(x)
+    return amigos
+    
+print(amigos(10000))
--- 1_Números amigos/Versión JS/amigosIterativo.js
+++ 1_Números amigos/Versión JS/amigosIterativo.js
@@ -2,32 +2,43 @@ console.time('loop');
 const start = Date.now();
 var n=100000;
 list=[];
-var x = 0;
+var x = 0; //almacena la sumatoria de divisores del primer valor
 var y = 0;
 var i=1;
 while (i <= n) {
-    var a=i;
+    
+    //almaceno en a el valor actual de i que es el número que estoy analizando
+    var a=i; //para i=220 => a=220
+    //Hallo la sumatoria de divisores del numero i que es el numero que estoy analizando
+    //y lo almaceno en x.
     for (var j = 1; j < i; j++) {
         if(i%j==0){
             x+=j;
         }
     }
+    //x tendrá el valor de la sumatoria de i.
     b=x;
+    //para i=220 => a=220 => x=248 => b=248
     if(a!=b){
+        //Calcula la sumatoria de divisores de B que anteriormente se le almacenó la sumatoria de divisores de i
         for(var k=1; k<b; k++){
             if(b%k==0){
                 y+=k;
             }
         }
     }
+
     if(a==y){
         list.push([a,b]);
         // si existe un numero amigo a la variable i que controla el ciclo principal le pasamos el numero amigo mayor para evitar 
         // controlar numeros amigos repetidos
+        // console.log(i);
         if(a>b){
             i=a;
+            console.log('nuevo valor de i',a)
         }else{
-            i=b;
+            i=b; 
+            console.log('nuevo valor de i',b)
         }
     }
     x=0; 
@@ -36,10 +47,11 @@ while (i <= n) {
 }
 const end=Date.now();
 console.timeEnd('loop');
-const time= end - start;
-const lista = document.getElementById('list');
-const tiempo = document.getElementById('time');
-list.forEach(item => {
-    lista.innerHTML += '<li>'+item+'</li>'
-});
-tiempo.innerHTML+='Tiempo de ejecución: '+time+' ms.';
+console.log(list)
+// const time= end - start;
+// const lista = document.getElementById('list');
+// const tiempo = document.getElementById('time');
+// list.forEach(item => {
+//     lista.innerHTML += '<li>'+item+'</li>'
+// });
+// tiempo.innerHTML+='Tiempo de ejecución: '+time+' ms.';
