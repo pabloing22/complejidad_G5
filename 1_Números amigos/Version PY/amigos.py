8b43d33cca97b1d5e41e9b18898d8bb3323904ee
pablitoing <pablito@circulo.digital>
2019-06-25 17:40:08 -0300

    ultima version nros amigos

--- 1_Números amigos/Version PY/amigos.py
+++ 1_Números amigos/Version PY/amigos.py
@@ -1,39 +1,41 @@
-from decorators import delta_time
+def divisores(nro):
+    sumdiv=0
+    max=nro
+    if(max>50):
+        if(nro%2==0):
+            max=(nro//2)+1
+        elif(nro%3==0):
+            max = (nro // 3) + 1
+        elif(nro%5==0):
+            max = (nro // 5) + 1
+        elif(nro%7==0):
+            max = (nro // 7) + 1
+        elif(nro%11==0):
+            max = (nro // 11) + 1
+
+    for (index,i) in enumerate(range(1,max)):
+        if(nro%i==0):
+            sumdiv=sumdiv+i
+    return sumdiv
 
 
-def allDivisores(n):
-    return ('todos los divisores de: ',n)
-
-
+n=10000
 def amigos(n):
-    divisores=allDivisores(n)
-    return divisores
 
-# print(amigos(100))
+    list=[]
+    x=0
+    y=0
 
-n=[220,284,1184,1210,2620,2924,5020,5564,6232,6368,10744,10856,12285,14595,17296,18416,63020,76084,66928,66992,67095,71145,69615,87633,79750,88730]
+    for j in range(220,n):
+        a=j
+        x=divisores(a)
+        b=x
+        if(a>b):
+            y=divisores(b)
+        if(a==y):
+            list.append([b,a])
+        x=0
+        y=0
+    return list
 
-prim=[2,3,5,7]
-
-
-def calc(n,arreglo):
-    if(n==1):
-        return arreglo
-    else:
-        m=n
-        if(n%2==0):
-            m=2
-        elif (n%3==0):
-            m=3
-        elif (n%5==0):
-            m=5
-        elif (n%7==0):
-            m=7
-        elif (n%11==0):
-            m=11
-        arreglo.append(m)
-        return calc(n//m,arreglo)
-
-
-
-print(calc(220,[]))
+print(amigos(n))
