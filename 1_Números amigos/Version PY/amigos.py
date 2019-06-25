c1f299ad5d57641ace32071ba6908c2ec961f11a
pablitoing <pablito@circulo.digital>
2019-06-25 01:08:10 -0300

    ejercicio 4 sudoku

--- 1_Números amigos/Version PY/amigos.py
+++ 1_Números amigos/Version PY/amigos.py
@@ -1,63 +1,39 @@
 from decorators import delta_time
 
 
-def divisores(n):
-    sum=0
-    # top=(n // 2)+1
-    # if(n % 2==0):
-    #     top=(n // 2)+1
-    # elif(n % 3 == 0):
-    #     top=(n // 3)+1
-    # elif (n % 5 == 0):
-    #     top=(n // 5)+1
-    # elif (n % 7 == 0):
-    #     top=(n // 7)+1
-    # elif (n % 11 == 0):
-    #     top=(n // 11)+1
-    ready=False
-    prim=[2,3,5,7,11,13,17,19,23,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,139,149,151,157,163,167,173,179,181]
-    tamaño=len(prim)
-    top=(n//2)+1
-    k=0
-    # este ciclo toma al numero y busca el menor valor primo divisible para obtener un rango de divisores y lo almacena en TOP
-    while not ready and k<tamaño:
-        if ((n % prim[k])==0):
-            top=(n//prim[k])+1
-            ready=True
-        k=k+1
-        
-    
-    for i in range(1,top):
-        if ((n%i)==0):
-            sum=sum+i
-    return sum
+def allDivisores(n):
+    return ('todos los divisores de: ',n)
 
 
-@delta_time('Grupo 5')
 def amigos(n):
-    # amigos() compara desde el 220 hasta n
-    # si encontró el par amigo [a,b] siendo siempre a<b
-    # b se guardará en founded y la próxima vez no se analizará b
-    # siempre se verifica que dado un numero i y la sumatoria de sus divisore i que z>i en caso contrario se descarta i
-    founded=[]
-    amigos=[]
-    for i in range(220,n):
-        if not(i in founded):
-            x=divisores(i)
-            if(x>i):
-                y=divisores(x)
-                if (i!=x) and (i==y):
-                    amigos.append([i,x])
-                    founded.append(x)
-    return amigos
-    
-print(amigos(10000))
+    divisores=allDivisores(n)
+    return divisores
 
-# @delta_time('Grupo 5')
-# def sumatoria1(n):
-#     for i in range(220,n+1):
-#         divisores(i)
+# print(amigos(100))
+
+n=[220,284,1184,1210,2620,2924,5020,5564,6232,6368,10744,10856,12285,14595,17296,18416,63020,76084,66928,66992,67095,71145,69615,87633,79750,88730]
+
+prim=[2,3,5,7]
 
 
-# sumatoria1(10000)
-# print(divisores(9973))
+def calc(n,arreglo):
+    if(n==1):
+        return arreglo
+    else:
+        m=n
+        if(n%2==0):
+            m=2
+        elif (n%3==0):
+            m=3
+        elif (n%5==0):
+            m=5
+        elif (n%7==0):
+            m=7
+        elif (n%11==0):
+            m=11
+        arreglo.append(m)
+        return calc(n//m,arreglo)
+
+
+
+print(calc(220,[]))
